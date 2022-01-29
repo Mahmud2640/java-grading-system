5874d5d707a3404a1dbee4b16e6e2739b907cd29
Mahmud2640 <mdmahmud484@gmail.com>
2022-01-29 15:06:05 +0600

    fix some problem

--- main.js
+++ main.js
@@ -5,21 +5,24 @@ function grade(){
       document.getElementById("view").innerHTML = "Enter Your Number";
    }
     else if (result <= 50) {
-      document.getElementById("view").innerHTML = "U got F";
+      document.getElementById("view").innerHTML = "You got F";
     }
     else if (result >= 50 && result < 60) {
-      document.getElementById("view").innerHTML = "U got D";
+      document.getElementById("view").innerHTML = "You got D";
     }
     else if (result >= 60 && result < 70) {
-      document.getElementById("view").innerHTML = "U got C";
+      document.getElementById("view").innerHTML = "You got C";
     }
     else if (result >= 70 && result < 80) {
-      document.getElementById("view").innerHTML = "U got B";
+      document.getElementById("view").innerHTML = "You got B";
     }
     else if (result >= 80 && result < 90) {
-      document.getElementById("view").innerHTML = "U got A";
+      document.getElementById("view").innerHTML = "You got A";
     }
     else if (result >= 90) {
-      document.getElementById("view").innerHTML = "U got A+";
+      document.getElementById("view").innerHTML = "You got A+";
+    }
+    else if (result >= 101) {
+      document.getElementById("view").innerHTML = "Input your valid number";
     }
 }
